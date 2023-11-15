6eaa2cfb5b6cc436d8f6e3f2209e2b144521ef51
Quentin Bonnafet <quentin.bonnafet@mysofie.fr>
2023-11-15 10:39:02 +0100

    lecture beyond crud

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
-  resources :restaurants
+  get 'reviews/new'
+  resources :restaurants do
+    resources :reviews, only: [:new, :create]
+    collection do
+      get :top
+    end
+    member do
+      get :chef
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
